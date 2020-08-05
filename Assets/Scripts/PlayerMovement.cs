de8dbd578352808295c76fe0bcaf5dac94ab9c17
toanvan <vanc.t123@gmail.com>
2020-08-06 01:26:17 +0700

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.walk;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.33f);
+        currentState = PlayerState.walk;
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        change.Normalize();
+        myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/RoomMove.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+    public Vector3 playerChange;
+    public Vector3 changePosition;
+    private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+    
+
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cam.minPosition = minPosition;
+            cam.maxPosition = maxPosition;
+            cam.transform.position = changePosition;
+            other.transform.position += playerChange;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
+        }
+    }
+
+    IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(2f);
+        text.SetActive(false);
+    }
+}
